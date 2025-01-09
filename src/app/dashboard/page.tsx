e086fca263fcd46f3ab9be9fad310d2773b80ac8
mdtanvirisl <mdtanvirislam945@gmail.com>
2025-01-10 00:00:23 +0600

    next js frontend structure

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,40 @@
+
+export default function Home() {
+    return (
+        <>
+            <h1>Hello World</h1>
+            <div className="grid grid-cols-2 gap-5 m-3">
+                <div className="card card-side bg-base-100 shadow-xl">
+                    <figure><img src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg" alt="Movie" /></figure>
+                    <div className="card-body">
+                        <h2 className="card-title">New movie is released!</h2>
+                        <p>Click the button to watch on Jetflix app.</p>
+                        <div className="card-actions justify-end">
+                            <button className="btn btn-primary">Watch</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="card card-side bg-base-100 shadow-xl">
+                    <figure><img src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg" alt="Movie" /></figure>
+                    <div className="card-body">
+                        <h2 className="card-title">New movie is released!</h2>
+                        <p>Click the button to watch on Jetflix app.</p>
+                        <div className="card-actions justify-end">
+                            <button className="btn btn-primary">Watch</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="card card-side bg-base-100 shadow-xl">
+                    <figure><img src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg" alt="Movie" /></figure>
+                    <div className="card-body">
+                        <h2 className="card-title">New movie is released!</h2>
+                        <p>Click the button to watch on Jetflix app.</p>
+                        <div className="card-actions justify-end">
+                            <button className="btn btn-primary">Watch</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+
+import Signin from "./signin/page";
+
+export default function Home() {
+  return (
+    <div>
+      <Signin/>
+    </div>
+  );
+}
